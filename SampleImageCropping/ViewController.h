abd58a59a232ea639a5ab462588b041601fd53ef
VISHAL GUPTA <vishal.gupta@intelligrape.com>
2015-04-17 12:45:35 +0530

    Initial Commit

---
+++ SampleImageCropping/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleImageCropping
+//
+//  Created by abhayam rastogi on 4/17/15.
+//  Copyright (c) 2015 Itelligrape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
